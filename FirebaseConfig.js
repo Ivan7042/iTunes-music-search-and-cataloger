684f8b782237f9eecee858f7a11172dea7ad9c81
Ivan Li <ivanli@umich.edu>
2024-11-30 02:28:40 -0500

    update

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 // Initialize Firebase with your config
 const firebaseConfig = {
   apiKey: "NA",
-  authDomain: "musiccataloger-e0e33.firebaseapp.com",
-  projectId: "musiccataloger-e0e33",
-  storageBucket: "musiccataloger-e0e33.firebasestorage.app",
-  messagingSenderId: "272077126137",
-  appId: "1:272077126137:web:75c70d261f4ea17352a528",
-  measurementId: "G-9H5R59KB9F"
+  authDomain: "NA",
+  projectId: "NA",
+  storageBucket: "NA",
+  messagingSenderId: "NA",
+  appId: "NA",
+  measurementId: "NA"
 };
 
 // Initialize Firebase app
